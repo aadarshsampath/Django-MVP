546ea8216b594a8fb99922ed09902fc476294234
Aadarsh <aadarsh.sampath@gmail.com>
2016-09-14 17:25:26 -0700

    Start the Django project

--- Django-MVP/DjangoApp/DjangoApp/urls.py
+++ Django-MVP/DjangoApp/DjangoApp/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'Django_MVP.views.home', name='home'),
+    # url(r'^$', 'DjangoApp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
