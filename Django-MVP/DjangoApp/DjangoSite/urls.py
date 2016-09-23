d1296ecf24082fc5c223a7bc50c1c4d375c8f0db
Aadarsh <aadarsh.sampath@gmail.com>
2016-09-23 14:54:25 -0700

    adding code for url to make use of static files.

--- Django-MVP/DjangoApp/DjangoSite/urls.py
+++ Django-MVP/DjangoApp/DjangoSite/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+#We are appending the static url to the url patterns for all the urls 
+# to make use of the static files.
 
 urlpatterns = [
     # Examples:
@@ -7,4 +12,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+] 
+if settings.DEBUG:
+	urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
